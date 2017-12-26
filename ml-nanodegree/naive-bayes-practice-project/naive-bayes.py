f44c44d3915028d9141c8bc9bc19f91db6247ad4
Thomas Lisankie <thomas.lisankie@protonmail.com>
2017-12-25 22:32:59 -0500

    Finished

--- ml-nanodegree/naive-bayes-practice-project/naive-bayes.py
+++ ml-nanodegree/naive-bayes-practice-project/naive-bayes.py
@@ -8,9 +8,6 @@ df = pd.read_table("smsspamcollection/SMSSpamCollection",
 
 df['label'] = df.label.map({'ham':0, 'spam':1})
 
-print(df.shape)
-print(df.head())
-
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.model_selection import train_test_split
@@ -47,4 +44,27 @@ stored in 'testing_data' using predict(). Save your predictions into the 'predic
 '''
 
 predictions = naive_bayes.predict(testing_data)
-print(predictions)
+
+'''
+Instructions:
+Compute the accuracy, precision, recall and F1 scores of your model using your test data 'y_test' and the predictions
+you made earlier stored in the 'predictions' variable.
+'''
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
+
+'''
+Conclusion:
+One of the major advantages that Naive Bayes has over other classification algorithms is its ability 
+to handle an extremely large number of features. In our case, each word is treated as a feature and there 
+are thousands of different words. Also, it performs well even with the presence of irrelevant features 
+and is relatively unaffected by them. The other major advantage it has is its relative simplicity. 
+Naive Bayes' works well right out of the box and tuning it's parameters is rarely ever necessary, except 
+usually in cases where the distribution of the data is known. It rarely ever overfits the data. Another 
+important advantage is that its model training and prediction times are very fast for the amount of data 
+it can handle. All in all, Naive Bayes' really is a gem of an algorithm!
+'''
