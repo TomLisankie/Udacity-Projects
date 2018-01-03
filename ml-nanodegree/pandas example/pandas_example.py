402f8dca7b21213d9044fda744807ac6bec16705
Thomas Lisankie <thomas.lisankie@protonmail.com>
2018-01-03 17:46:02 -0500

    -

--- ml-nanodegree/pandas example/pandas_example.py
+++ ml-nanodegree/pandas example/pandas_example.py
@@ -6,4 +6,6 @@ dates = pd.date_range("20130101", periods = 6)
 
 df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list("ABCD"))  #our DataFrame
 
-print(df)
+print(df["A"])
+
+print(df[df.A > 0])
