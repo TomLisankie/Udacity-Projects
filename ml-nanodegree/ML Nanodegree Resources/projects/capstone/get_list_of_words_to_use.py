70444c45b47d650496e2de6b430d8ed6398dfc37
Thomas Lisankie <thomas.lisankie@protonmail.com>
2018-03-07 13:29:00 -0500

    -

--- ml-nanodegree/ML Nanodegree Resources/projects/capstone/get_list_of_words_to_use.py
+++ ml-nanodegree/ML Nanodegree Resources/projects/capstone/get_list_of_words_to_use.py
@@ -2,8 +2,23 @@ import pandas as pd
 import numpy as np
 
 np.random.seed(172632)
+
+# shuffle and shorten
 phonemic_transcriptions_frame = pd.read_csv("phonemic_transcriptions.csv")
-print(phonemic_transcriptions_frame.shape)
-# phonemic_transcriptions_list = phonemic_transcriptions_frame.iloc[0].values
-# shuffled = np.random.shuffle(phonemic_transcriptions_list)
-# print(shuffled)
+phonemic_transcriptions_list = phonemic_transcriptions_frame.iloc[:, 1:2].values
+np.random.shuffle(phonemic_transcriptions_list)
+shuffled_shortened = phonemic_transcriptions_list[0 : 5000]
+
+# create train and test sets
+train = list(shuffled_shortened[:4000])
+test = list(shuffled_shortened[4000:])
+
+# save train and test sets as CSV files
+import csv
+with open('train.csv', 'w') as csvfile:
+    the_writer = csv.writer(csvfile)
+    the_writer.writerows(train)
+
+with open('test.csv', 'w') as csvfile:
+    the_writer = csv.writer(csvfile)
+    the_writer.writerows(test)
